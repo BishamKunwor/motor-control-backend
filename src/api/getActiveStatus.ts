e58df7caf39d9bdda4ee1e37e4762c5e24892250
Bisham Kunwor <novelian.nova@gmail.com>
2023-05-05 10:38:51 +0545

    Get app state directly from server

--- src/api/getActiveStatus.ts
+++ src/api/getActiveStatus.ts
@@ -1,9 +1,10 @@
-// import { getBlynkActiveStatus } from "../controller/blynkController";
+import { getBlynkActiveStatus } from "../controller/blynkController";
 import { getActiveDate } from "../database/date";
-import { getSwitchStats } from "../database/switchStats";
+import { getSwitchStats, setSwitchStats } from "../database/switchStats";
 
 export default async function getActiveStatusHandler(req: any, res: any) {
-  // const status = await getBlynkActiveStatus();
+  const value = await getBlynkActiveStatus();
+  setSwitchStats(value);
   const date = getActiveDate();
   res.status(200).json({ status: getSwitchStats(), time: date });
 }
