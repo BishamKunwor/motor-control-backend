754d71ab67dafc29b30f279f1c289ab718a3f5ca
Bisham Kunwor <novelian.nova@gmail.com>
2023-05-07 12:06:58 +0545

    Initial Web socket setup

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,30 @@ import setSwitchStatsHandler from "./api/setSwitchStats";
 import updateSwitchHandler from "./api/updateSwitch";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
+const port: number = 8080;
 
 const app: Application = express();
-const port: number = 3000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, { serveClient: false });
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Successfully Running");
+  res.send("Successfully Running with web-sockets");
 });
 
-app.get("/api/getActiveStatus", getActiveStatusHandler);
-app.get("/api/getReloadStatus", getReloadStatusHandler);
-app.post("/api/setReloadStatus", setReloadStatusHandler);
-app.get("/api/setSwitchStats", setSwitchStatsHandler);
-app.post("/api/updateSwitch", updateSwitchHandler);
+// app.get("/api/getActiveStatus", getActiveStatusHandler);
+// app.get("/api/getReloadStatus", getReloadStatusHandler);
+// app.post("/api/setReloadStatus", setReloadStatusHandler);
+// app.get("/api/setSwitchStats", setSwitchStatsHandler);
+// app.post("/api/updateSwitch", updateSwitchHandler);
 
-app.listen(port);
+// app.listen(port);
+httpServer.listen(port, () => {
+  console.log(`Server is Listening on port ${port}...`);
+});
