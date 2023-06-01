6354841dc437ad4a9fcd3f3482059f067175c349
Bisham Kunwor <novelian.nova@gmail.com>
2023-06-01 16:27:09 +0545

    Resolved Type err

--- src/api/updateSwitch.ts
+++ src/api/updateSwitch.ts
@@ -8,6 +8,7 @@ export default function updateSwitchHandler(req: any, res: any) {
     if (req.body.status === "ON") {
       setActiveDate(req.body.time);
     }
+    //@ts-ignore please check this for future update`
     updateSwitch(req.body?.status, req.body?.time);
     const newDate = getActiveDate();
     res.status(200).json({
